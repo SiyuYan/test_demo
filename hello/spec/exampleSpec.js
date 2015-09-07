7d322d8698162e526c011d79a3f51aae9a9f8dbd
syyan <syyan@thoughtworks.com>
2015-09-07 19:01:30 +0800

    selenium rc

--- hello/spec/exampleSpec.js
+++ hello/spec/exampleSpec.js
@@ -11,25 +11,17 @@
 //driver.quit();
 //
 //
-
-describe('e2e test', function() {
-  it('should get a correct page', function() {
-     browser.get('http://localhost:3000/home');
-    // element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-    // element(by.css('[value="add"]')).click();
-
-    // var todoList = element.all(by.repeater('todo in todoList.todos'));
-    // expect(todoList.count()).toEqual(3);
-    // expect(todoList.get(2).getText()).toEqual('write first protractor test');
-
-    // // You wrote your first test, cross it off the list
-    // todoList.get(2).element(by.css('input')).click();
-    // var completedAmount = element.all(by.css('.done-true'));
-    console.log('dd');
-    console.log(browser.getTitle);
-    expect(browser.getTitle()).toEqual('home');
-    element(by.name('btn')).click();
-   // expect(completedAmount.count()).toEqual(2);
-  });
+describe('angularjs homepage', function () {
+    it('should get correct pages', function (done) {
+        browser.driver.get('http://localhost:3000/home');
+        expect(browser.driver.getTitle()).toEqual('home');
+        done();
+    });
+    it('should return baidu when click button', function (done) {
+        browser.driver.get('http://localhost:3000/home');
+        browser.driver.findElement(by.name('btn')).click();
+        expect(browser.driver.getTitle()).toEqual('百度一下，你就知道');
+        done();
+    });
 });
 
